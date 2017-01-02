5e77abe0de1759fce57f477de267b56faa57a1e6
David Froger <david.froger@mailoo.org>
2017-01-02 08:10:39 +0100

    add pydebug feature

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -15,6 +15,7 @@ config=$(cat <<CONDARC
 channels:
  - conda-forge
  - defaults # As we need conda-build
+ - dfroger  # As we need pydebug
 
 conda-build:
  root-dir: /feedstock_root/build_artefacts
